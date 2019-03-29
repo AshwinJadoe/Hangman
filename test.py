031bf8f33e7dc078694cae63ecfe677d5716641b
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-03-29 18:25:20 +0100

    checkt letters en voegt ze toe

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import random
 
 lives_allowed = 11
 lives = 0
-letters = [""]
+letters = ["a"]
 wordlist = [
 "lion", "umbrella", "window", "computer", "glass", "juice", "chair", "desktop",
  "laptop", "dog", "cat", "lemon", "cabel", "mirror", "hat"
            ]
 blanks = [""]
-
+nieuw = False
 
 
 print ("Welcome to hangman")
@@ -17,8 +17,31 @@ word = random.choice(wordlist)
 
 lengte = len(word)
 
-
 for i in range(lengte):
     blanks.append("_")
+    
+print (' '.join(map(str, blanks)))
 
-print (''.join(map(str, blanks)))
+
+
+
+while lives_allowed > lives:
+    guess = input("Choose a letter: ")
+    if len(guess) > 1:
+        print ("please enter 1 letter")
+    if guess.isalpha() == False:
+        print ("please only use letters")
+    if guess.lower() in letters :
+        print("you've already guessed that letter")
+    elif guess.lower != letters :
+        print (letters)
+        letters.append(guess)
+     
+
+
+        
+    
+    
+    
+
+    
